c9ef03aec60bf3603aa402d0b68bba80bbc3d4d1
Crislaine Furquim <crislaine.furquim@hotmail.com>
2024-02-26 11:41:23 -0500

    first commit

--- app.py
+++ app.py
@@ -4,22 +4,25 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles.csv') #lendo os dados
 hist_button = st.button('Criar histograma') #criar um botão
+graf_button = st.button('Criar gráfico de dispersão') #criar um botão
 
 st.header('Clique no botão acima para iniciar')
 
 if hist_button: #se o botão for clicado
     #escrever uma mensagem
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    
+if graf_button: #se o botão for clicado
+    #escrever uma mensagem
+    st.write('Criando gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
     #criar um histograma
-    fig = px.histogram(car_data, x="odometer")
+fig = px.histogram(car_data, x="odometer")
 
     #exibir um gráfico Plotly interativo
-    st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
     
-    graf_button = st.button('Criar gráfico de dispersão') #criar um botão
-    
-    fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
+fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
     
     #exibir um gráfico Plotly interativo
-    st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
