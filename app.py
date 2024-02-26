1b92e4105815166d027c33d05009479456d78ce3
Crislaine Furquim <crislaine.furquim@hotmail.com>
2024-02-25 21:03:41 -0500

    first commit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ hist_button = st.button('Criar histograma') #criar um botão
 
 st.header('Clique no botão acima para iniciar')
 
+
 if hist_button: #se o botão for clicado
     #escrever uma mensagem
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
@@ -16,3 +17,9 @@ if hist_button: #se o botão for clicado
 
     #exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
+
+# criar uma caixa de seleção
+build_histogram = st.checkbox('Criar um histograma')
+
+if build_histogram: # se a caixa de seleção for selecionada
+st.write('Criando um histograma para a coluna odometer')
